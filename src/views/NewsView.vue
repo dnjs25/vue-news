cb79ecde881ab67e79632521d54cbe19d32a0d91
yewon <won29255@naver.com>
2019-02-26 17:36:28 +0900

    api call

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    news
+    <div v-for="user in users">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    var vm = this;
+    axios.get('	https://api.hnpwa.com/v0/news/1.json')
+      .then(response => vm.users = response.data)
+      .catch(error => console.log(error))
+  }
 }
 </script>
 
