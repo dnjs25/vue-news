d25b605a1e17a6ba39a56bb569b30b0a85dc9ecf
yewon <won29255@naver.com>
2019-03-05 17:44:02 +0900

    [실습] router

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <p v-for="item in fetchedAsk">
-      <a :href="item.url">
+      <router-link v-bind:to="`/item/${item.id}`">{{ item.title }}</router-link>
+      <!-- <a :href="item.url">
         {{ item.title }}
-      </a>
+      </a> -->
       <small>{{ item.time_ago }} by {{ item.user }}</small>
     </p>
   </div>
