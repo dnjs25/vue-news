832c9e975f5f4cc6a94d35915bafd58b1b2ad927
yewon <won29255@naver.com>
2019-03-18 16:13:51 +0900

    리팩토링3 - spinner

---
+++ src/utils/bus.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+
+// export const bus = new Vue();
+// App.vue
+// import { bus } from './bus.js';
+
+export default new Vue();
+
+// App.vue
+// import Bus from './bus.js';
